44ce61f6895a731b9cca64ae1e52c7414aebff66
mafl20 <mafl20@student.aau.dk>
2025-05-20 16:23:34 +0200

    Started work on Evaluator.py

---
+++ evaluator.py
@@ -0,0 +1,67 @@
+import torch
+import numpy as np
+
+from scipy.stats import gamma
+from torch.nn.functional import mse_loss
+
+class Evaluator:
+    def __init__(self):
+        pass
+
+    def evaluate_model(self, model, input_data, device):
+        output_features = []
+        mse_list = []
+
+        model.eval()
+        total_mse = 0.0
+        total_samples = 0
+
+        with torch.no_grad():
+            for data in input_data:
+                input = data.to(device)
+                output = model(input)
+
+                output_features.append(output.cpu().numpy())  # Store the reconstruction for later use
+
+                mse = mse_loss(output, input, reduction='sum').item()
+                mse_list.append(mse)
+                total_mse += mse
+                total_samples += input.numel()
+                
+        average_mse = total_mse / total_samples
+
+        return average_mse, output_features
+    
+    def bundle(self, data, original_clip_lengths):
+        clips = []
+
+        start_index = 0
+
+        for size in original_clip_lengths:
+            end_index = start_index + size
+            clips.append(data[start_index:end_index])
+            start_index = end_index
+
+        return clips
+    
+    def reconstruction_error(self, original, reconstructed):
+        recon_err_per_clip = []
+
+        for i in range(len(original)):
+            original_sample = original[i].reshape(-1)
+            reconstructed_sample = reconstructed[i].reshape(-1)
+            
+            recon_err_per_clip.append(np.mean((original_sample - reconstructed_sample)**2))
+
+        return recon_err_per_clip
+    
+    def gamma_distribution(self, error):
+        shape, loc, scale = gamma.fit(error)
+
+        x = np.linspace(0, max(error), 1000)
+        gamma_pdf = gamma.pdf(x, shape, loc=loc, scale=scale)
+
+        anomaly_threshold = gamma.ppf(0.9, shape, loc=loc, scale=scale)
+        
+        
+
